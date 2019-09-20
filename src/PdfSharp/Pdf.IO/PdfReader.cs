203a19e30697bc72ece71edd618e6c1b1f1f86a9
Toni Leitold <toni@voidpointer.com>
2019-09-20 16:05:57 +0200

    Ignore token errors

--- src/PdfSharp/Pdf.IO/PdfReader.cs
+++ src/PdfSharp/Pdf.IO/PdfReader.cs
@@ -439,7 +439,8 @@ namespace PdfSharp.Pdf.IO
                         {
                             Debug.WriteLine(ex.Message);
                             // 4STLA rethrow exception to notify caller.
-                            throw;
+                            // throw;
+
                         }
                     }
                     else
